ec8c3b299f6ba948f145f7e5b2828bf2705d1573
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-09-07 15:12:43 -0400

    more Css

--- css_frameworks.js
+++ css_frameworks.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function(){ //just need this first line once!
      $('.carousel').carousel();
+     $(".button-collapse").sideNav();
+    //  console.log("HellOO")
+    
    });
-       
